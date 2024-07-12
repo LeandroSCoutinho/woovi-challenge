a7e1a6dc7d23792524ea88002f770ae14d75d2a4
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2024-07-11 22:36:17 -0300

    feat: routes config

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppRouter = () => {
+    return (
+        <Routes>
+            <Route path="pagina-inicial" element={<p>Página inicial</p>}/>
+           
+            <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+        </Routes>
+    );
+}
